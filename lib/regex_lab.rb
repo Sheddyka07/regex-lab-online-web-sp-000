53c9d548d6e2e3a299f8325b9d49484845584805
Kelly Sheddy <jungaka07@gmail.com>
2018-11-19 15:53:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^un\w+ing/)
-     return text 
-  else 
-    return false
-  end
+  uning_words = text.match(/^un\w+ing/)
+  
 end
 
 def words_five_letters_long(text)
