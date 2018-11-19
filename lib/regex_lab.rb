3d1a5e95c76576a3de5b28eec2a67887370e8394
Kelly Sheddy <jungaka07@gmail.com>
2018-11-19 15:52:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if word.match(/\A[un]/) && word.match(/\z[ing]/)
-     return true 
+  if text.match(/^un\w+ing/)
+     return text 
   else 
     return false
   end
